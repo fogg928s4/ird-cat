41a04fff0c612c16857918bb116826de1e03d6e0
J Melg <rjmzelaya@gmail.com>
2024-10-16 23:22:33 -0600

    Last commit for 20241016

--- ird.py
+++ ird.py
@@ -1,8 +1,12 @@
-for i in range(20, 60,10):
+for i in range(10, 60,10):
     print(f'int gi0/0.{i}', end ='\n')
     print('ip access-group 123 in', end='\n')
     print('ex', end='\n')
     
 for i in range(20, 60,10):
     print(f'access-list 1 permit 192.168.{i}.0 0.0.0.255', end ='\n')
-   
+  
+for i in range(10, 60,10):
+    print(f'int gi0/0.{i}', end ='\n')
+    print('ip nat inside', end='\n')
+    print('ex', end='\n')
